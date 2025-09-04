09f72f4ec38aededaadc10127991fc65dcd0946b
globemediaofficial <109893647+globemediaofficial@users.noreply.github.com>
2025-09-03 17:44:50 -0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "nsfw-server",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "nsfwjs": "^2.4.0",
+    "@tensorflow/tfjs-node": "^4.9.0",
+    "canvas": "^2.12.1",
+    "multer": "^1.4.5"
+  }
+}
